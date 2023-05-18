7090f51adc38673b1f54fa7eb98a0c7b7bca8ce3
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-18 06:51:30 +0100

    adding loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import Card from "./Card/Card";
+import Loading from "./Loading/Loading";
+
 function App() {
   return (
     <div className="App">
       <Card />
+      <Loading />
     </div>
   );
 }
